643e5c07af0a21b78325a945df7c1c59ade7e0d9
Otto Roming <otto.roming@protonmail.com>
2026-01-18 23:12:34 +0100

    fixed register form

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -13,7 +13,7 @@
 	<div class="grid grid-cols-[1fr_auto_1fr] gap-4">
 		<div>
 			<h1 class="pb-2 pl-1 text-xl font-semibold">Login</h1>
-			<form action="?/register" use:enhance class="flex flex-col gap-2">
+			<form method="post" action="?/register" use:enhance class="flex flex-col gap-2">
 				<Input label="username" name="username" type="text" />
 				<Input label="password" name="password" type="password" />
 				<Button type="submit">Login</Button>
