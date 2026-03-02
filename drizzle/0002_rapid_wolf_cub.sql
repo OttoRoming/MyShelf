e14868044645cde90d56f32a75062708329c0741
Otto Roming <otro@lysator.liu.se>
2026-03-02 10:00:39 +0100

    go rewrite

--- drizzle/0002_rapid_wolf_cub.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `book` ADD `olid` text;--> statement-breakpoint
-CREATE UNIQUE INDEX `book_olid_unique` ON `book` (`olid`);
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OttoRoming/MyShelf
+
+go 1.25.0
